b6cdbec80abea13fd85b5084cf0f41e2dcb83b75
slozhkai <114486246+slozhkai@users.noreply.github.com>
2023-09-06 14:18:45 +0300

    Add files via upload

---
+++ Практические работы по C++/Практическая 1/Задание 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int qwqqq()
+{   
+    double x = -4.5, y = -.75e-4, z = -0.845e2;
+    double first = cbrt(9. + pow(x - y, 2)) / (pow(x, 2) + pow(y, 2)  + 2.) ;
+    double second = exp(abs(x-y)) * pow(tan(z), 3);
+    cout<< first - second;
+
+    return 0;
+}
