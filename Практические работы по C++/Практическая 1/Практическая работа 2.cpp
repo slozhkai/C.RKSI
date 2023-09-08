ceb4f5ca35aa7f8595fed9a9f3a507bc7829d7c4
slozhkai <114486246+slozhkai@users.noreply.github.com>
2023-09-08 20:21:46 +0300

    Add files via upload

---
+++ Практические работы по C++/Практическая 1/Практическая работа 2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+ 
+ 
+int main()
+{
+    double x=0,y=0,s=0;
+    cout<<"Введите два числа через пробел: ";
+    cin>>x>>y;
+    if(y>2*x){
+        for(int i=1;i<=20;i++)
+            s=(tan(x)+pow(x,2));
+    }
+
+    else if(y<2*x){
+        for(int i=1;i<=10;i++)
+            s=pow(abs(x+y),3);
+    }
+
+    else{
+        s=pow(abs(pow(y,3)-pow(x,2)),1/3);
+    }
+    cout<<s;
+    return 0;
+}
